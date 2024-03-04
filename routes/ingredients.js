856a42371611ee41aa41011ec367241602824b26
Irina Zotova <zotovaiv87@gmail.com>
2024-03-03 21:45:31 -0500

    Get all ingredients

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -4,27 +4,23 @@ var router = express.Router();
 const Ingredient = require("../modals/ingredient");
 const errors = require("./errors");
 
-/* GET ingredients listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get("/getAllIngredients", async function (req, res, next) {
+  const data = await Ingredient.find({});
+  res.send(200, data);
 });
 
-/* Save new ingredients listing. */
-router.post("/", function (req, res, next) {
+router.post("/", async function (req, res, next) {
   if (req.body.name) {
-    const ingredient = new Ingredient({
-      name: req.body.name,
-      description: req.body.description,
-    });
-    ingredient
-      .save()
-      .then((val) => {
-        res.send(200, val);
-      })
-      .catch((e) => {
-        console.log("Error save", e);
-        res.send(500, errors.ERROR_SAVE_DATA);
+    try {
+      const ingredient = await Ingredient.create({
+        name: req.body.name,
+        description: req.body.description,
       });
+      res.send(200, ingredient);
+    } catch (e) {
+      console.log("Error save", e);
+      res.send(500, errors.ERROR_SAVE_DATA);
+    }
   } else {
     res.send(400, errors.INVALID_DATA);
   }
